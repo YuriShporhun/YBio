31c8da319c5e3c758bdcbc04ddfc2b7e768df174
YuriShporhun <yuripclive@live.ru>
2015-07-01 00:27:39 +0300

    .

--- Source/YSeq.py
+++ Source/YSeq.py
@@ -1,30 +1,29 @@
 class YSeq:
     """This class represents a sequence"""
 
-    #The sequence of the letters of an alphabet
-    _sequence = []
-
     #Constructor makes a copy of sequence
     def __init__(self, sequence):
         """Constructor constructs sequence as list
         :param sequence: the sequence of the letters of some alphabet
         :type sequence: str
         """
-        self._sequence = sequence[:]
+        self._sequence = []
+        self._sequence.extend(sequence)
     
-    def Count(self, symbol):
+    def count(self, symbol):
         """This method returns the number of characters in the sequence as int
         :param symbol: symbol or sequence of some alphabet
+        :type symbol: str
         """
         return self._sequence.count(symbol)
 
-    def Append(self, symbol):
-        '''This method adds a character or sequence
+    def append(self, symbol):
+        """This method adds a character or sequence
         symbol: symbol or sequence of some alphabet
-        '''
+        """
         self._sequence.extend(symbol)
 
-    def Save(self, filename, separator = ''):
+    def save(self, filename, separator = ''):
         '''This method saves a sequence in the file
         filename: is path to the file
         separator: is a string which will added to each symbol in the sequence
@@ -44,12 +43,9 @@ class YSeq:
     def __getitem__(self, index):
         return self._sequence[index]
 
-    def Load(self, filename):
-        '''This method loads a sequence from file
-        filename: is path to the file
-        '''
+    def load(self, filename):
+        """This method loads a sequence from file
+        :param filename: is path to the file
+        """
         with open(filename) as file:
             [self._sequence.extend(x.rstrip('\n')) for x in file]
-
-    def LongestCommonSubstring(self, sequence):
-        pass
--- UnitTests/test_YSeq.py
+++ UnitTests/test_YSeq.py
@@ -4,29 +4,30 @@ from YSeq import YSeq
 __author__ = 'Yuri Shporhun'
 
 class TestYSeq(TestCase):
+
     def test_Count(self):
         #The test sequence has to have 3 A, 4 T, 5 C and 6 G
         test_sequence = "AAATTTTCCCCCGGGGGG"
         seq = YSeq(test_sequence)
-        a_count = seq.Count("A")
-        t_count = seq.Count("T")
-        c_count = seq.Count("C")
-        g_count = seq.Count("G")
-        self.assertEqual(a_count, 3, "Actual A count is 3")
-        self.assertEqual(t_count, 4, "Actual T count is 4")
-        self.assertEqual(c_count, 5, "Actual C count is 5")
-        self.assertEqual(g_count, 6, "Actual G count is 6")
+        a_count = seq.count("A")
+        t_count = seq.count("T")
+        c_count = seq.count("C")
+        g_count = seq.count("G")
+        self.assertEqual(a_count, 3)
+        self.assertEqual(t_count, 4)
+        self.assertEqual(c_count, 5)
+        self.assertEqual(g_count, 6)
 
     def test_Append(self):
         #The test sequence has to contain ATG
         test_sequence = "ATG"
         result_sequence = "ATGA"
         seq = YSeq(test_sequence)
-        seq.Append("A")
-        self.assertEqual(seq, result_sequence)
+        seq.append("A")
+        self.assertEqual(str(seq), result_sequence)
 
     def test_Save(self):
         pass
 
     def test_Load(self):
-       pass
+        pass
